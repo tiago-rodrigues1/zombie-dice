06001afebadaee2f22c4dea782e67dbe5f1bdff7
tiago-rodrigues1 <tiagords127@gmail.com>
2025-06-24 12:53:52 -0300

    refactor: dice faces enum

--- src/zdie.cpp
+++ src/zdie.cpp
@@ -1,15 +1,24 @@
 #include <random>
+#include <unordered_map>
 
 #include "zdie.hpp"
 
 Zdie::Zdie(dice_type_e t, std::string f) : type(t), faces(f) {}
 
-char Zdie::roll() {
+ZdieFaces Zdie::roll() {
   std::random_device rd;
   std::mt19937 gen(rd());
 
   std::uniform_int_distribution<> distrib(0, (faces.size() - 1));
   auto idx = distrib(gen);
 
-  return faces[idx];
+  char face = faces[idx];
+
+  std::unordered_map<char, ZdieFaces> face_map{
+    { 'b', ZdieFaces::BRAIN },
+    { 's', ZdieFaces::SHOTGUN },
+    { 'f', ZdieFaces::FOOTPRINT },
+  };
+
+  return face_map[face];
 }
